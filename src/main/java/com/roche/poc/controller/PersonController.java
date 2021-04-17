c7a5b87cd912b795ddf06b1fcbe1ec57bf72ac25
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2021-04-17 18:33:21 +0200

    test deploy

--- src/main/java/com/roche/poc/controller/PersonController.java
+++ src/main/java/com/roche/poc/controller/PersonController.java
@@ -4,14 +4,7 @@ import com.roche.poc.entity.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("person")
@@ -40,7 +33,7 @@ public class PersonController {
 
     @DeleteMapping("/{id}")
     ResponseEntity<Void> deletePerson(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
